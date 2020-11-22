0be2f581df9cc12e7abc2946035fccedf283a856
Alexandr <stefanitsky.mozdor@gmail.com>
2020-11-22 15:08:26 +0300

    :sparkles: imporve websocket notifications / add pydantic

--- openwiden/repositories/messages.py
+++ openwiden/repositories/messages.py
@@ -6,5 +6,5 @@ REPOSITORY_ALREADY_REMOVED = _("Repository already removed.")
 NOT_ADDED_REPOSITORY_CANNOT_BE_REMOVED = _("Not added repository cannot be removed.")
 REPOSITORY_DOES_NOT_EXIST = _("Repository with id {id} does not exist.")
 
-REPOSITORY_IS_ADDED = _("Repository is added.")
-REPOSITORY_IS_REMOVED = _("Repository is removed.")
+REPOSITORY_IS_ADDED = _("Repository {name} is added.")
+REPOSITORY_IS_REMOVED = _("Repository {name} is removed.")
